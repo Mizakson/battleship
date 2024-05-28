387aa58b1d822721a959211f5839089eaf89c590
Mizakson <maxwellizakson@gmail.com>
2024-05-28 14:52:57 -0400

    Create initializeGrid method

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,6 +2,23 @@ import Ship from "../src/ship"
 
 export default class Gameboard {
     constructor() {
-        
+        this.coords = this.initializeGrid()
     }
+
+    initializeGrid() {
+        let grid = []
+        
+        let x = 10
+        let y = 10
+
+        for (let i = 0; i < x; i++) {
+            grid[i] = []
+            for (let j = 0; j < y; j++) {
+                grid[i][j] = 0
+            }
+        }
+
+        return grid
+    }
+    
 }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,4 +1,9 @@
 import Ship from "../src/ship"
 import Gameboard from "../src/gameboard"
 
-const TEST = new Gameboard()
+const DUMMY = new Gameboard()
+
+test("grid creation", () => {
+    expect(DUMMY.coords.length).toBe(10)
+    expect(DUMMY.coords[0].length).toBe(10)
+})
