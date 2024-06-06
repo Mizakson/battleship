ef92e4f97cd831c384102fe83117a605858e7fa5
Mizakson <maxwellizakson@gmail.com>
2024-06-06 10:13:58 -0400

    Delete Work Sans font pack

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import Gameboard from "../src/gameboard"
 import Player from "../src/player"
 // dom-events input statement here
 import uiMethods from "../src/interface"
+import "../src/css/styles.css"
+
+uiMethods.formatMainDisplay()
+uiMethods.createMainHeader()
