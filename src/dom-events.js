bdf5fd22b8ab5463eaf2c6416d45a00df8bd5be4
Mizakson <maxwellizakson@gmail.com>
2024-06-12 11:00:54 -0400

    Create empty domMethods factory

--- src/dom-events.js
+++ src/dom-events.js
@@ -1,3 +1,11 @@
 import Ship from "../src/ship"
 import Gameboard from "../src/gameboard"
 import Player from "../src/player"
+
+const domMethods = ( function() {
+
+    // TODO: create openLinkInNewTab onclick event
+
+})()
+
+export default domMethods
