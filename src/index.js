c578fcc2a0652ad5e77d7cf812ae5b1bb1a45ae2
Mizakson <maxwellizakson@gmail.com>
2024-07-08 14:45:33 -0400

    Start refactoring of user interface

--- src/index.js
+++ src/index.js
@@ -3,37 +3,4 @@ import Gameboard from "../src/gameboard"
 import Player from "../src/player"
 import domMethods from "../src/dom-events"
 import uiMethods from "../src/interface"
-import MainGame from "../src/model"
 import "../src/css/styles.css" 
-
-// bool -- if true app loads
-const APP_STATE = true
-
-// Keep header and main containers constantly on the page
-function skeleton() {
-    if (APP_STATE === true) {
-        uiMethods.mainHeader()
-        uiMethods.mainContent()
-        console.log("MAIN PAGE LOADED...")
-    }
-    if (APP_STATE === false) console.log(APP_STATE)
-}
-skeleton()
-
-// load grids only if skelelton called
-function basicGridRender() {
-    uiMethods.createGrids()
-    console.log("BASIC GRIDS LOADED...")
-}
-
-function init() {
-    if (skeleton) {
-        basicGridRender()
-        const game = new MainGame()
-        // console.log(game.human)
-        // console.log(game.cpu)
-    }
-}
-
-// main program init func
-init()
