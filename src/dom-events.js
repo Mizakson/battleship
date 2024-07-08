98fd7d5106d663a898c292498758a144f797152e
Mizakson <maxwellizakson@gmail.com>
2024-07-08 11:49:28 -0400

    Remove openLinkInNewTab function

--- src/dom-events.js
+++ src/dom-events.js
@@ -3,10 +3,7 @@ import Gameboard from "../src/gameboard"
 import Player from "../src/player"
 
 const domMethods = {
-    "openLinkInNewTab": (url) => {
-        window.open(url, '_blank')
-    },
-
+    
 }
 
 export default domMethods
