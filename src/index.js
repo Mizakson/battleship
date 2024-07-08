c916018a3177979d503d7a46fa7d89ba4522ce6f
Mizakson <maxwellizakson@gmail.com>
2024-07-08 15:23:41 -0400

    Refactor interface.js, index.js, styles.css

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import Player from "../src/player"
 import domMethods from "../src/dom-events"
 import uiMethods from "../src/interface"
 import "../src/css/styles.css" 
+
+// ui excluding grid rendering
+const UI_STATE = true
+
+const baseInterface = (function() {
+    if (UI_STATE) {
+        uiMethods.skeleton()
+    }
+    if (!UI_STATE) return false
+})()
--- src/interface.js
+++ src/interface.js
@@ -23,6 +23,45 @@ const uiMethods = {
         return el
 
     },
+
+    "mainTitle": function() {
+        const titleContainer = uiMethods.createContainer("main-title", "")
+        const titleText = uiMethods.createEl("h2", "title-text", "", "Battleship")
+        titleContainer.appendChild(titleText)
+
+        uiMethods.body.appendChild(titleContainer)
+    },
+
+    "projectDesc": function() {
+        const descContainer = uiMethods.createContainer("project-desc", "")
+        descContainer.innerHTML = `<p class="desc-text"> 
+        <a href="https://www.theodinproject.com" id="odin-project-url" title="https://www.theodinproject.com">Odin Project</a> JavaScript Course Final Project
+        </p>`
+        
+        document.querySelector(".main-title").appendChild(descContainer)
+    },
+
+    "projectCredit": function() {
+        const credContainer = uiMethods.createContainer("project-cred", "")
+        credContainer.innerHTML = `<p class="cred-text"> A program by
+        <a href="https://github.com/mizakson" id="github-url" title="https://github.com/mizakson">Mizakson</a>
+        </p>`
+        
+        document.querySelector(".main-title").appendChild(credContainer)
+    },
+
+    "contentContainer": function() {
+        const content = uiMethods.createContainer("content", "")
+        uiMethods.body.appendChild(content)
+    },
+
+    // wrapper function for title, proj description
+    "skeleton": function() {
+        uiMethods.mainTitle()
+        uiMethods.projectDesc()
+        uiMethods.projectCredit()
+        uiMethods.contentContainer()
+    }
     
 }
     
